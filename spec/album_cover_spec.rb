1182331ee57d3ad9dd31aa77009981c42935b419
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-17 15:39:14 +0000

    Automatically backed up by Learn

--- spec/album_cover_spec.rb
+++ spec/album_cover_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'HTML Album Cover' do
         expect(title_box.children.any? {|ch| ch.name == 'h4'}).to be == true
         expect(title_box.children.select {|ch| ch.name == 'h4'}.first.children.first.text).to be == ("PHARRELL WILLIAMS"), "Your 'h4' tag should contain the text 'PHARRELL WILLIAMS'"
 
-        expect(title_box.children.any? {|ch| ch.name == 'h2'}).to be == 'true', "No 'h2' tag found"
+        expect(title_box.children.any? {|ch| ch.name == 'h2'}).to be == true
         expect(title_box.children.select {|ch| ch.name == 'h2'}.first.children.first.text).to be == "HAPPY", "Your 'h2' tag should contain the text 'HAPPY'"
 
         expect(title_box.children.select {|ch| ch.name == 'h4'}[1].children.first.text).to include("FROM DESPICABLE ME"), "A second 'h4' tag should contain the text 'FROM DESPICABLE ME'"
